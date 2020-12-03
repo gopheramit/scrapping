4178d6316c08e4bf523b95e8fde167bda9be2595
gopheramit <amitagrawal10@outlook.com>
2020-12-03 22:20:53 +0530

    custmizing header

--- main.go
+++ main.go
@@ -5,10 +5,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
-	response, err := http.Get("https://www.devdungeon.com/")
+
+	client := &http.Client{
+		Timeout: 30 * time.Second,
+	}
+	request, err := http.NewRequest("GET", "https://www.devdungeon.com/", nil)
+
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	request.Header.Set("User-Agent", "test")
+
+	response, err := client.Do(request)
 	if err != nil {
 		log.Fatal(err)
 
