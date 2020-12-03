528e8e642d9d8c4a9faa05c58519d776f31b5134
gopheramit <amitagrawal10@outlook.com>
2020-12-03 13:51:55 +0530

    making a get request

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	response, err := http.Get("https://www.devdungeon.com/")
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	defer response.Body.Close()
+
+	n, err := io.Copy(os.Stdout, response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Data", n)
+}
