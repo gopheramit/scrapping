8fb3fe9f5ae172d7a22d52e31ffc5140b9bbb008
gopheramit <amitagrawal10@outlook.com>
2020-12-16 20:20:07 +0530

    goquery

--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
-	"net/http"
-	"os"
-	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
+func postScrape() {
+	doc, err := goquery.NewDocument("http://jonathanmh.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("#main article .entry-title").Each(func(index int, item *goquery.Selection) {
+		title := item.Text()
+		linkTag := item.Find("a")
+		link, _ := linkTag.Attr("href")
+		fmt.Printf("Post #%d: %s - %s\n", index, title, link)
+	})
+
+}
+
+func linkScrape() {
+	doc, err := goquery.NewDocument("http://jonathanmh.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// use CSS selector found with the browser inspector
+	// for each, use index and item
+	doc.Find("body a").Each(func(index int, item *goquery.Selection) {
+		linkTag := item
+		link, _ := linkTag.Attr("href")
+		linkText := linkTag.Text()
+		fmt.Printf("Link #%d: '%s' - '%s'\n", index, linkText, link)
+	})
+}
+
+func main() {
+	postScrape()
+	linkScrape()
+}
+
+/*
 func main() {
 	//downloadUrl()
 	//addr := flag.String("addr", "www.google.com", "Website to be scarepped")
@@ -37,7 +73,6 @@ func main() {
 
 	log.Println("Data", n)
 }
-
 func downloadUrl() {
 	response, err := http.Get("https://www.devdungeon.com/archive")
 	if err != nil {
@@ -57,4 +92,4 @@ func downloadUrl() {
 		log.Fatal(err)
 	}
 
-}
+}*/
